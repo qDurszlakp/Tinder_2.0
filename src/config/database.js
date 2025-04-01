92108811b83adebf9640be6c779c854fc8283bee
durszlak <pawel8marshall@gmail.com>
2025-04-02 01:40:09 +0200

    Changes yolo

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,18 @@ let db;
 // Asynchroniczna funkcja do połączenia z MongoDB
 async function connectDB() {
   try {
-    const client = new MongoClient(mongoUrl);
+    // Opcje połączenia dla rozwiązania problemów z TLS
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+      tls: true,
+      tlsInsecure: true,
+      tlsAllowInvalidCertificates: false,
+      retryWrites: true
+    };
+
+    const client = new MongoClient(mongoUrl, options);
     await client.connect();
     db = client.db(dbName);
     console.log(`Połączono z bazą danych MongoDB: ${dbName}`);
